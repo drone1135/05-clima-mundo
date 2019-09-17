4008eef49d9df2ba27966cf96fe6cd3e0dd41d32
dcarazo <dario_carazo@hotmail.com>
2019-09-17 17:30:09 -0300

    mejora de los parámetros del yargs

--- app.js
+++ app.js
@@ -1,12 +1,6 @@
 const lugar = require('./lugar/lugar');
 const clima = require('./clima/clima');
-const argv = require('yargs').options({
-    direccion: {
-        alias: 'd',
-        desc: 'Direccion de la ciudad para obtener el clima',
-        demand: true
-    }
-}).argv;
+const argv = require('./yargs/yargs').argv;
 
 const getInfo = async(direccion) => {
     try {
